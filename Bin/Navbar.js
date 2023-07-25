d2e29710409e89bfc9afe62a698ac1de2c438f6f
ishanlamsalGameDEV <devdasiila@gmail.com>
2023-07-25 22:07:06 +0545

    New

---
+++ Bin/Navbar.js
@@ -0,0 +1,62 @@
+function DarkMode_() {
+    var Icon = document.getElementById("_DarkModeICON").innerHTML;
+    var Dark = document.getElementById("_darkMode");
+    var Dark_Childs = document.getElementsByClassName("DarkMode");
+    if (Icon == "dark_mode") {
+        for (let index = 0; index < Dark_Childs.length; index++) {
+            const element = Dark_Childs[index];
+            element.style.backgroundColor = "rgba(40,40,40,1)"
+            element.style.color = "#FFF"
+        }
+        document.getElementById("_DarkModeICON").innerHTML = "sunny";
+        Dark.style.transform = "translate(-5px, 5px)";
+        Dark.style.color = "#FFF";
+        Dark.style.backgroundColor = "green";
+        Dark.style.border = "None";
+        Dark.style.boxShadow = "None";
+        Dark.childNodes[0].nextSibling.style.backgroundColor = "green";
+
+
+
+    } else {
+        for (let index = 0; index < Dark_Childs.length; index++) {
+            const element = Dark_Childs[index];
+            element.style.backgroundColor = "#FFF"
+            element.style.color = "rgba(40,40,40,1)"
+        }
+        document.getElementById("_DarkModeICON").innerHTML = "dark_mode";
+        Dark.style.transform = "translate(0px, 0px)";
+        Dark.style.color = "#black";
+        Dark.style.backgroundColor = "white";
+        Dark.style.border = "1px solid rgba(0, 0, 0, 0.2)";
+        Dark.style.boxShadow = " -5px 5px rgba(0, 0, 0, 0.5)";
+        Dark.childNodes[0].nextSibling.style.backgroundColor = "#FFF"
+
+    }
+
+
+}
+
+function Home() {
+    var HomePannel = document.getElementById("HomePannel");
+    var PlayerPannel = document.getElementById("PlayerPannel");
+    if (HomePannel.style.display == "block") {
+        HomePannel.style.display = "none";
+        PlayerPannel.style.display = "block";
+    } else {
+        HomePannel.style.display = "block"
+        PlayerPannel.style.display = "none";
+    }
+}
+
+function Player() {
+    var HomePannel = document.getElementById("HomePannel");
+    var PlayerPannel = document.getElementById("PlayerPannel");
+    if (PlayerPannel.style.display == "block") {
+        PlayerPannel.style.display = "none";
+        HomePannel.style.display = "block"
+    } else {
+        PlayerPannel.style.display = "block"
+        HomePannel.style.display = "none"
+    }
+}
